e3f3d3995a1d8e85e5bfa977769ea13691189bfc
Brad <klausmickelson52@gmail.com>
2024-10-10 20:01:51 +0100

    feat: add `ai` for summarizing and generating contents

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -11,9 +11,26 @@ export class FeedController {
     response: Response
   ): Promise<void> {
     try {
-      const { limit, status } = request.query;
-      const feed = await feedService.getFeed(Number(limit));
+      const { page, limit } = request.query;
+      const feed = await feedService.getFeed(Number(page || 1), Number(limit));
       response.status(200).json(feed);
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        response.status(error.statusCode).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: "Internal Server Error" });
+      }
+    }
+  }
+
+  public async getFeedSummary(
+    request: ExtendedRequest,
+    response: Response
+  ): Promise<void> {
+    try {
+      const { limit } = request.query;
+      const summary = await feedService.getRecentFeedSummary(Number(limit));
+      response.status(200).json({ summary });
     } catch (error: any) {
       if (error instanceof AppError) {
         response.status(error.statusCode).json({ message: error.message });
